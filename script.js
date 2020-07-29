fe0eb82584e5b203eb0c1ade3083cc1b03b673ed
Kevin M. Smith <smithkevin1100@gmail.com>
2020-07-29 13:44:04 +0100

    Started redraw function

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ const reset = () => {
             elem.style.border = '1px solid black;'
     });
 }
+const redraw = () => {
+    gridSize = document.getElementById('myInput').value;
+    document.getElementById('game').innerHTML = ''
+    for (let index = 0; index < (gridSize*gridSize); index++) {
+        document.getElementById('game').innerHTML += '<div id="grid-element"></div>'
+        
+    }
+    elementsArray = document.querySelectorAll("#grid-element");
+
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("mouseover", function() {
+        elem.style.backgroundColor = '#FFAD1E'
+        elem.style.border = '1px solid #874A05;'
+        
+    });
+});
+}
