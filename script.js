67e6df93626b3407a7fab3064c65aa8baa9f1515
Kevin M. Smith <smithkevin1100@gmail.com>
2020-07-29 13:18:45 +0100

    basic fucntionality working

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ for (let index = 0; index < (16*16); index++) {
     document.getElementById('game').innerHTML += '<div id="grid-element"></div>'
     
 }
+let elementsArray = document.querySelectorAll("#grid-element");
+
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("mouseover", function() {
+        elem.style.backgroundColor = '#FFAD1E'
+        elem.style.border = '1px solid #874A05;'
+        
+    });
+});
+const sketch = () => {
+
+}
